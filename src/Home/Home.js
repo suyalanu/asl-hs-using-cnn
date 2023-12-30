1489b79dca724a24c17db9594f07feeb4c5da720
Anushka suyal <anushka@Anushkas-MacBook-Air.local>
2023-12-30 23:58:39 +0545

    feat: create a home page including header and footer

---
+++ src/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../Css/Home.css";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
